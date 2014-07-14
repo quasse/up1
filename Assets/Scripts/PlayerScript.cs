ba3187a22d03dc7a496ee8d063498b35d6e48bd3
Sam Morgen <quasse@gmail.com>
2014-07-14 13:40:48 -0500

    More Stuff
    Most stuff. Now I'm stuck.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -38,7 +38,29 @@ public class PlayerScript : MonoBehaviour {
 		}
 		
 	}
-	
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		bool damagePlayer = false;
+		
+		// Collision with enemy
+		EnemyScript enemy = collision.gameObject.GetComponent<EnemyScript>();
+		if (enemy != null)
+		{
+			// Kill the enemy
+			HealthScript enemyHealth = enemy.GetComponent<HealthScript>();
+			if (enemyHealth != null) enemyHealth.Damage(enemyHealth.hp);
+			
+			damagePlayer = true;
+		}
+		
+		// Damage the player
+		if (damagePlayer)
+		{
+			HealthScript playerHealth = this.GetComponent<HealthScript>();
+			if (playerHealth != null) playerHealth.Damage(1);
+		}
+	}
+
 	void FixedUpdate()
 	{
 		// 5 - Move the game object
---
+++ Assets/Scripts/RendererExtensions.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RendererExtensions : MonoBehaviour {
+
+	public static bool IsVisibleFrom(this Renderer renderer, Camera camera)
+	{
+		Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+		return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+	}
+}
